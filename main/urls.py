4bf55b050316fb6ac4f962179871bd567fc5756b
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-04-23 01:26:38 +0530

    Added Dummy Data

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ShoeList, ShoeDetail,index
+from .views import ShoeList, ShoeDetail,index,dummy_data
 
 urlpatterns = [
+    path('dummy',dummy_data),
     path('',index),
     path('shoes/', ShoeList.as_view()),
     path('shoes/<int:pk>/', ShoeDetail.as_view()),
--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from .serializers import ShoeSerializer
 from .models import Shoe
 from rest_framework import generics
+from faker import Faker
+import random
+
 class ShoeList(generics.ListCreateAPIView):
     queryset = Shoe.objects.all()
     serializer_class = ShoeSerializer
@@ -14,3 +17,35 @@ class ShoeDetail(generics.RetrieveUpdateDestroyAPIView):
 
 def index(req):
     return render(req,'index.html')
+
+def dummy_data(request):
+    fake = Faker()
+
+    # List of possible values for gender
+    genders = ['Men', 'Women', 'Unisex']
+
+    # List of possible values for material
+    materials = ['Leather', 'Synthetic', 'Canvas', 'Mesh']
+
+    # List of possible values for style
+    styles = ['Running', 'Casual', 'Formal', 'Sports', 'Sneakers']
+
+    # List of possible colors
+    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Black', 'White']
+
+    # Generate 50 dummy Shoe objects
+    for i in range(50):
+        shoe = Shoe(
+            model_name=fake.word(),
+            color=random.choice(colors),
+            size=random.randint(6, 13),
+            price=random.randint(50, 200),
+            material=random.choice(materials),
+            style=random.choice(styles),
+            description=fake.text(),
+            photo=fake.image_url(),
+            gender=random.choice(genders),
+        )
+        shoe.save()
+    message = 'Dummy data created successfully.'
+    return render(request,'dummydata.html', {'message': message})
